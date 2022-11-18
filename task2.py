656b283c1ea9bfa57184568f37f46092325ab8cd
ameneahani <118308026+ameneahani@users.noreply.github.com>
2022-11-18 22:41:17 +0330

    Delete task2.py

--- task2.py
+++
@@ -1,9 +0,0 @@
-a=float(input("enter the first side of the triangle:"))
-b=float(input("enter the second side of the triangle:"))
-c=float(input("enter the third side of the triangle:"))
-if a<(b+c):
-    if b<(a+c):
-        if c<(a+b):
-            print("yes")
-else:
-    print("no")
